ba6f7e453ae72f39783e89a6d016c609559900dc
wannabegamedev <olinelson@outlook.com>
2025-07-26 15:09:53 +0100

    UI and Skybox Fix

---
+++ Assets/Resources/SkyboxFix.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class SkyboxFix : MonoBehaviour
+{
+   [SerializeField] private Material skyboxShader;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+  
+    
+}
